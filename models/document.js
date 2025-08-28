08f3e907e5e000e5869f3d1c68b05230af04820b
Azankham <Azan786kha@gmail.com>
2025-08-29 01:59:14 +0530

    Clean backend, remove conflicts, add start script

--- models/document.js
+++ models/document.js
@@ -10,17 +10,6 @@ const documentSchema = new mongoose.Schema({
   profitLoss: { type: [String], required: false },
   voidedCheck: { type: [String], required: false },
   userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "user" }, // Ensure userId is valid ObjectId
-<<<<<<< HEAD
-=======
-  location: {
-    type: {
-      latitude: { type: Number, required: true },
-      longitude: { type: Number, required: true },
-      address: { type: String, required: true }
-    },
-    required: true
-  },
->>>>>>> d8d1103331e05cc34b6c93f8548f244473142803
 });
 
 module.exports = mongoose.model("Documents", documentSchema);
